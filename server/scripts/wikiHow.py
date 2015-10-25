28266d27c2a2b1f9e1a3861805337179d65e102e
Liam Kirsh <liam.kirsh@riseup.net>
2015-10-25 05:34:52 -0700

    more error handling

--- server/scripts/wikiHow.py
+++ server/scripts/wikiHow.py
@@ -5,20 +5,28 @@ from fuzzywuzzy import process
 from bs4 import BeautifulSoup as bs
 import re
 
-def is_step(tag):
-	return tag('li') and not tag.previous_element('ul')
+def throw_err():
+	print
+	sys.stdout.flush()
+	sys.exit(0)
 
+if len(sys.argv) < 2:
+	throw_err()
 query = sys.argv[1].lower()
+if not query:
+	throw_err()
 r = requests.get('http://wikihow.com/api.php?action=query&list=search&srsearch=%s&utf8=&format=json' % query)
 search_results = r.json()
 search_results = [results["title"] for results in search_results["query"]["search"]]
+if not search_results:
+	throw_err()
 
 # remove how to from query
 s_query = query.replace("how to ", "")
 best_title = process.extractOne(s_query, search_results)[0].encode('ascii','replace')
 #print best_title
 if not best_title:
-	sys.exit(100)
+	throw_err()
 
 r = requests.get('http://wikihow.com/api.php?action=parse&page=%s&section=1&prop=text&format=json' % best_title)
 steps = r.json()
@@ -32,6 +40,8 @@ soup = bs(steps, 'html.parser')
 #print soup.prettify()
 #sys.exit(0)
 method_1 = soup.find('ol')#.contents
+if not method_1:
+	throw_err()
 #print method_1
 #sys.exit(0)
 print json.dumps([best_title] + ([step.contents[0].encode('utf-8').strip() for step in method_1.find_all('li')]))
